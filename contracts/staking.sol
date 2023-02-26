55ca49c6d16170fe50fc5ddb7837af99fc8cec8e
dunnie99 <oluwadunnirachael@gmail.com>
2023-02-26 13:44:42 +0100

    updateScript

--- contracts/staking.sol
+++ contracts/staking.sol
@@ -23,7 +23,7 @@ contract gatedStaking {
 
     constructor() {
         boredApeContract = IERC721(0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D);
-        rewardToken = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+        stakeToken = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
         admin = msg.sender;
     }
 
--- scripts/staking.ts
+++ scripts/staking.ts
@@ -1,10 +1,12 @@
 import { ethers, network } from "hardhat";
+import { time } from "@nomicfoundation/hardhat-network-helpers";
+
 
 async function main() {
     const [owner] = await ethers.getSigners();
 
     const ApeHolder1 = "0x4A385286592C97e457A6f54A3734557F4b095A28";
-    const NonApeHolder1 = "0xDa9CE944a37d218c3302F6B82a094844C6ECEb17";
+    const NonApeHolder = "0xDa9CE944a37d218c3302F6B82a094844C6ECEb17";
     // Deploy reward contract
     const Token = await ethers.getContractFactory("CSRtoken");
     const token = await Token.deploy("Caesar", "CSR");
@@ -24,6 +26,9 @@ async function main() {
     await helpers.impersonateAccount(ApeHolder1);
     const impersonatedSigner = await ethers.getSigner(ApeHolder1);
 
+    await helpers.impersonateAccount(NonApeHolder);
+    const impersonatedSigner1 = await ethers.getSigner(NonApeHolder);
+
 
     const Usdt = await ethers.getContractAt("IUSDT", "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48");
     await helpers.setBalance(ApeHolder1, 10000000000000000000000000);
@@ -35,15 +40,29 @@ async function main() {
     //const amount = ethers.utils.parseEther("0.5");
     const userBalanceB4 = await Usdt.balanceOf(impersonatedSigner.address);
     console.log(`BALANCE BEFORE STAKING ${userBalanceB4}`);
+    // const allowance = await Usdt.allowance(impersonatedSigner.address, staking.address)
+    // console.log(`Contract allowance is ${allowance}`);
+    
 
-    //staking
+    //staking BoredApe HOlder
 
-    const stakes = ethers.utils.parseEther("0.01");
-    const stake1 = await stakingContract.connect(impersonatedSigner).stake(stakes);
+    // stakes = ethers.utils.parseEther("0.01");
+    const stake1 = await stakingContract.connect(impersonatedSigner).stake(20000000);
     console.log(`address one staked successfully`);
     const userBalanceAfter = await Usdt.balanceOf(impersonatedSigner.address);
 
-    console.log(`BALANCE BEFORE STAKING ${userBalanceAfter}`);
+    console.log(`BALANCE AFTER STAKING ${userBalanceAfter}`);
+
+
+
+    /// Non BoredApe Holder
+
+    // const stake2 = await stakingContract.connect(impersonatedSigner1).stake(20000000);
+    // console.log(`address one staked successfully`);
+    // const userBalanceAfter1 = await Usdt.balanceOf(impersonatedSigner1.address);
+
+    // console.log(`BALANCE AFTER STAKING ${userBalanceAfter1}`);
+
 
 
 
